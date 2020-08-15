79ad50c68b60be3c74285e62e7c631de1f5997a3
Rosny Ratsianompo <rosny.rat@onja.org>
2020-08-15 12:00:58 +0100

    I daisplay the list of book and worked on adding a new book

--- script.js
+++ script.js
@@ -1,40 +1,80 @@
 const books = [{
-        tilte: 'Ny angano',
-        author: 'S.william',
+        title: 'Ny angano',
+        author: 'Dox',
         genre: 'Fantasy',
-        numbers: 300,
-        status: read,
+        pages: 300,
+        status: true,
     },
     {
-        tilte: 'Ny vano',
-        author: 'Peter',
+        title: 'Ny vano',
+        author: 'patrick',
         genre: 'Mystery',
-        numbers: 800,
-        status: not,
+        pages: 800,
+        status: false,
     },
     {
-        tilte: 'Ny rano',
+        title: 'Ny rano',
         author: 'jean Narivony',
         genre: 'Design',
-        numbers: 100,
-        status: read,
+        pages: 100,
+        status: true,
     },
 ];
 
+const myLibrary = document.querySelector('.form');
+const listForm = document.querySelector('.list-form');
 const bookLibrary = document.querySelector('.book-library');
-const listOfBook = document.querySelector('.list-of-book');
+const listOfBook = document.querySelector('.lists-of-book');
+const submitBtn = document.querySelector('.add-button');
 
 // Generate the element using .map();
-const html = books.map(book => {
-        return `
-        <li>
+const displayBooks = () => {
+    const html = books.map(book =>
+            `<li class="book-list">
             <span>${book.title}</span>
             <span>${book.author}</span>
             <span>${book.genre}</span>
-            <span>${book.numbers}</span>
-            <span>${book.status}</span>
-        </li>
-    `;
-    })
-    .join(" ");
-listOfBook.innerHTML = html;
+            <span>${book.pages}</span>
+            <input value="${book.status}"
+            ${book.complete ? 'checked' : '' } 
+            type="checkbox">
+            <span>
+                <img class="icon" src="./delete-24px.png"/>
+            </span>
+        </li>`)
+        .join(" ");
+    listOfBook.insertAdjacentHTML('beforeend', html);
+};
+displayBooks();
+
+let library = [];
+
+const handleSubmit = e => {
+    e.preventDefault(); // prevent the page from loading
+    const myBook = e.currentTarget.newBook;
+    // if it is empty, then don't submit it.
+    if (!myBook) return;
+    console.log(myBook);
+
+    const newBook = {
+        title: title,
+        author: author,
+        genre: genre,
+        pages: pages,
+        status: false,
+    };
+    // push it into our state
+    library.push(newBook);
+    // clear the form
+    e.target.reset();
+
+    // fire off a custom event that will tell anyone else who cares
+    listOfBook.dispatchEvent(new CustomEvent('libraryUpdated'));
+};
+
+const handleButtonSubmit = e => {
+    e.preventDefault();
+
+}
+myLibrary.addEventListener('submit', handleSubmit);
+submitBtn.addEventListener('submit', handleSubmit)
