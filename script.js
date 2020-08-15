d8793704db175545185b5d25f1bd37d7f87c80ad
Rosny Ratsianompo <rosny.rat@onja.org>
2020-08-15 17:14:50 +0100

    I worked on the delete button and checkbox but not solved yet

--- script.js
+++ script.js
@@ -3,21 +3,21 @@ const books = [{
         author: 'Dox',
         genre: 'Fantasy',
         pages: 300,
-        status: true,
+        status: "read",
     },
     {
         title: 'Ny vano',
         author: 'patrick',
         genre: 'Mystery',
         pages: 800,
-        status: false,
+        status: "not",
     },
     {
         title: 'Ny rano',
         author: 'jean Narivony',
         genre: 'Design',
         pages: 100,
-        status: true,
+        status: "read",
     },
 ];
 
@@ -36,10 +36,11 @@ const displayBooks = () => {
             <span>${book.genre}</span>
             <span>${book.pages}</span>
             <input value="${book.status}"
-            ${book.complete ? 'checked' : '' } 
+            ${book.status === "read" ? 'checked' : '' } 
             type="checkbox">
             <span>
-                <img class="icon" src="./delete-24px.png"/>
+            <button class="remove" arial-label="Remove">x
+        </button>
             </span>
         </li>`)
         .join(" ");
@@ -51,17 +52,19 @@ let library = [];
 
 const handleSubmit = e => {
     e.preventDefault(); // prevent the page from loading
-    const myBook = e.currentTarget.newBook;
-    // if it is empty, then don't submit it.
-    if (!myBook) return;
-    console.log(myBook);
+    const myBook = e.currentTarget;
+    const title = myBook.titleBook.value;
+    const author = myBook.authorBook.value;
+    const pages = myBook.pagesBook.value;
+    const genre = myBook.genreBook.value;
+    const status = myBook.statusBook.value;
 
     const newBook = {
         title: title,
         author: author,
         genre: genre,
         pages: pages,
-        status: false,
+        status: "read",
     };
     // push it into our state
     library.push(newBook);
@@ -72,9 +75,68 @@ const handleSubmit = e => {
     listOfBook.dispatchEvent(new CustomEvent('libraryUpdated'));
 };
 
-const handleButtonSubmit = e => {
-    e.preventDefault();
-
+const handleNewbook = () => {
+    const newBookhtml = library.map(item =>
+            `<li class="book-list">
+                <span>${item.title}</span>
+                <span>${item.author}</span>
+                <span>${item.genre}</span>
+                <span>${item.pages}</span>
+                <input value="${item.status}"
+                ${item.status === "read" ? 'checked' : '' } 
+                type="checkbox">
+                <span>
+                    <button class="remove" arial-label="Remove 
+                        ${item.title} 
+                        ${item.author}
+                        ${item.genre}
+                        ${item.pages}"
+                    value="${item.id}">&times;
+                    </button>
+                </span>
+        </li>`)
+        .join(" ");
+    listOfBook.innerHTML += newBookhtml;
 }
+
+const deleteBook = id => {
+    console.log('deleting book', id);
+    library = library.filter(libraries => libraries.id !== id);
+    listOfBook.dispatchEvent(new CustomEvent('libraryUpdated'));
+};
+
+const markAsRead = id => {
+    console.log(id);
+    const bookRead = library.library.find(libraries => libraries.id === id);
+    bookRead.status = !bookRead.status;
+    listOfBook.dispatchEvent(new CustomEvent('libraryUpdated'));
+};
+// Set an item to localStorage
+const mirrorToLocalStorage = () => {
+    console.log('mirroring book to local storage');
+    localStorage.setItem('library', JSON.stringify(library));
+};
+
+const restoreFromLocalStorage = () => {
+    console.log('Restoring from Ls(localStorage)');
+    const localBook = JSON.parse(localStorage.getItem('library'));
+    if (localBook) {
+        library.push(...localBook);
+    };
+    listOfBook.dispatchEvent(new CustomEvent('libraryUpdated'));
+};
+
 myLibrary.addEventListener('submit', handleSubmit);
-submitBtn.addEventListener('submit', handleSubmit)
+listOfBook.addEventListener('libraryUpdated', handleNewbook);
+listOfBook.addEventListener('libraryUpdated', mirrorToLocalStorage);
+
+listOfBook.addEventListener('click', function(e) {
+    const id = e.currentTarget.value;
+    if (e.target.matches('.remove')) {
+        deleteBook(id);
+    }
+    if (e.target.matches('input[type="checkbox"]')) {
+        console.log('marking as complete', id);
+    }
+});
+restoreFromLocalStorage();
